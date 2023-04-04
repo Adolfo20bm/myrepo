43527a940286f8d7d9c699978ea8511b5c452a66
Adolfo20bm <abm2491@gmail.com>
2023-04-04 12:46:31 -0500

    botonmenu y efecto de hablidades javascript

---
+++ script.js
@@ -0,0 +1,41 @@
+let menuVisible = false;
+//Funsion que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    }else{
+        //responsive de seccion responsive el nav.responsive
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //Oculto el menu una ve que selecciona una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >=300 ){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("bootstrap");
+        habilidades[4].classList.add("php");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajoequipo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+//detecto el scrolling para aplicar la animacion de la barra habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
